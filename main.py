b340987ac6911f733a39bab60edcf905399a780e
Axel Daniel <77642568+AxelOP96@users.noreply.github.com>
2025-07-02 17:13:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-    "https://frontend-alquimia.vercel.app"],  
+    "https://frontend-alquimia.vercel.app/buscar-nota-perfume"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
