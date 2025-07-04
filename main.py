17f31c70037a866b4dcd5d4b1de5f66d0e34d57f
Axel Daniel <77642568+AxelOP96@users.noreply.github.com>
2025-07-04 15:31:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def get_perfume_image(url: str = Query(...)):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
+    chrome_options.add_argument("--remote-debugging-port=9222")
     #service = Service("./chromedriver.exe")
     #driver = webdriver.Chrome(service=service, options=chrome_options)
     #service = Service(ChromeDriverManager().install()) #
