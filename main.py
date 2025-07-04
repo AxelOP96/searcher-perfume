e2f754efbcbfe1c1cdefafde768ba78fe4922ebc
Axel Daniel <77642568+AxelOP96@users.noreply.github.com>
2025-07-04 14:14:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def get_perfume_image(url: str = Query(...)):
     chrome_options = Options()
     #chrome_options.binary_location = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"  
     #chrome_options.add_argument("--headless=new")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"#
+    #chrome_options.binary_location = "/usr/bin/chromium-browser"#
     chrome_options.add_argument("--headless")#
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
